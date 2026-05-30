a5297db53ba8d3890b47ce879329dc7bb009f7ae
Adham Akmal Azmi <adham_92@live.com>
2026-05-31 03:06:42 +0800

    Remove unused files and update project metadata in Storybook configuration

--- storybook-static/assets/structuredCloneButFunctions-DQbCKoDN.js
+++
@@ -1,1 +0,0 @@
-import"./i18n-CjAMWY9o.js";function e(t){return!(t instanceof Object)||typeof t==`function`?t:t instanceof Array?t.map(e):Object.fromEntries(Object.entries(t).map(([t,n])=>[t,e(n)]))}export{e as t};
